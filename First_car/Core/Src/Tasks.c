84367c6c71a59144cd882cd6a146915ceaa2b963
Mohamed Bendary Ibrahim <146913381+Mohamed-Bendary@users.noreply.github.com>
2024-02-23 23:56:47 +0200

    Update Tasks.c

--- First_car/Core/Src/Tasks.c
+++ First_car/Core/Src/Tasks.c
@@ -5,6 +5,26 @@
  *      Author: dell
  */
 
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+#include "Buzzer.h"
+#include "LCD_I2C.h"
+#include "GPS.h"
+#include "bluetooth.h"
 
-/*aaaaaaaaaaaaaaaaaaaaaaaaah*/
+
+void TASK_LCDBuzzer (void *parameters)
+{
+
+	while(1)
+	{
+
+
+	}
+
+
+}
+
+
 
